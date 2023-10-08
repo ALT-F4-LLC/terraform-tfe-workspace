499f00588506d648cdca2fef79b670a1469665a2
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2023-10-07 23:17:27 -0700

    feat: added support for run triggers

--- flake.nix
+++ flake.nix
@@ -1,29 +1,28 @@
 {
   description = "terraform-tfe-workspace";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         packages = {
           terraform-with-plugins = pkgs.terraform.withPlugins (ps: with ps; [
             tfe
           ]);
 
-          default = pkgs.runCommand "default" {
-            src = ./.;
-          } ''
+          default = pkgs.runCommand "default"
+            {
+              src = ./.;
+            } ''
             mkdir -p $out
             cp -R $src/*.tf $out
 
             ${config.packages.terraform-with-plugins}/bin/terraform -chdir="$out" init
             ${config.packages.terraform-with-plugins}/bin/terraform -chdir="$out" validate
           '';
-          };
+        };
 
         devShells = {
           default = pkgs.mkShell {
